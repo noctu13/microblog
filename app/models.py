19798c6f04f587e97598f12a92da8d462cb45378
Сапралиев Михаил Евгеньевич < @eslaero.net>
2025-12-10 19:46:41 +0300

    Chapter 6: Profile and Avatars

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from datetime import datetime, timezone
+from hashlib import md5
 from typing import Optional
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
@@ -15,6 +16,9 @@ class User(UserMixin, db.Model):
         sa.String(120), index=True, unique=True)
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(
         sa.String(256))
+    about_me: so.Mapped[Optional[str]] = so.mapped_column(sa.String(140))
+    last_seen: so.Mapped[Optional[datetime]] = so.mapped_column(
+        default=lambda: datetime.now(timezone.utc))
     
     posts: so.WriteOnlyMapped['Post'] = so.relationship(
         back_populates='author')
@@ -27,6 +31,10 @@ class User(UserMixin, db.Model):
         
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+    
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return f'https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}'
 
 
 
